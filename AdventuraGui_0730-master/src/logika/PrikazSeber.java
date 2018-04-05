b84391f369966c3c64e60b59e407e686beb919d4
baka03 <36510584+baka03@users.noreply.github.com>
2018-04-05 15:03:50 +0200

    Update PrikazSeber.java

--- AdventuraGui_0730-master/src/logika/PrikazSeber.java
+++ AdventuraGui_0730-master/src/logika/PrikazSeber.java
@@ -6,7 +6,7 @@ package logika;
  *  Třída PrikazSeber implementuje pro hru příkaz seber.
  *  Tato třída je součástí jednoduché textové hry.
  *  
- * @author    Margarita Tsakunova
+ * @author    Anastasiia Bakhareva
  * @version   pro školní rok 2016/2017
  */
 
